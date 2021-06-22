888f3482f276721918969c8c9807c17c8798dc77
Diósney <diosneyf.gomes@gmail.com>
2021-06-21 23:06:50 -0300

    Projeto Front-End Para Processo Seletivo Bee IT

---
+++ src/BlazorApp/ViewModels/NumeroViewModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorApp.ViewModels
+{
+    public class NumeroViewModel
+    {
+        [Key]
+        public Guid Id { get; set; }
+
+        [Required]
+        [Range(1, int.MaxValue, ErrorMessage = ("O menor número que pode ser digitado é 1"))] 
+        public int Valor { get; set; }
+        
+        public int Resultado { get; set; }
+        
+        public string Expressao { get; set; }
+    }
+}
